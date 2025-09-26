464e2e9c419098197a2920fbdbdf8b4ea3c8245e
Bhavya <Bhavyagupta2302@gmail.com>
2025-09-26 10:06:06 +0530

    Create First api (signup)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const connectDB = require("./config/database")
 const app =express();
+const User = require('./models/user')
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    console.log(req.body);
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("User added successfully!")
+    } catch(err){
+        res.status(400).send("Error saving the user: "+ err.message);
+    }
+})
 
 connectDB()
 .then(() => {
